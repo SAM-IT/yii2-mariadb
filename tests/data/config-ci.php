a0016be4e7b6de7adc031f5e912efb73bc8c7e3c
Sam Mousa <sam@mousa.nl>
2020-05-06 18:39:28 +0200

    ci: use port from env

--- tests/data/config-ci.php
+++ tests/data/config-ci.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 return \yii\helpers\ArrayHelper::merge(require 'config-docker.php', [
     'databases' => [
         'mysql' => [
-            'dsn' => 'mysql:host=127.0.0.1;dbname=yiitest',
+            'dsn' => 'mysql:host=127.0.0.1;port=' .getenv('PORT'). ';dbname=yiitest',
         ]
     ]
 ]);
