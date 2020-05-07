24c2fb061d8677df82f8aa23b7b48e19130ce251
Sam Mousa <sam@mousa.nl>
2020-05-07 15:13:07 +0200

    test(ci): use correct configuration when running in ci

--- tests/data/config-ci.php
+++ tests/data/config-ci.php
@@ -1,10 +1,13 @@
 <?php
 declare(strict_types=1);
 
-return \yii\helpers\ArrayHelper::merge(require 'config-docker.php', [
-    'databases' => [
-        'mysql' => [
-            'dsn' => 'mysql:host=127.0.0.1;port=' .getenv('PORT'). ';dbname=yiitest',
-        ]
+$config = [
+    'class' => \yii\db\Connection::class,
+    'dsn' => 'mysql:host=127.0.0.1;port=' .getenv('PORT'). ';dbname=yiitest',
+    'username' => 'root',
+    'password' => '',
+    'schemaMap' => [
+        'mysql' => \SamIT\Yii2\MariaDb\Schema::class
     ]
-]);
+];
+return $config;
